1b8983d940e1f41b0d0e250fd3a3f662a714bc7d
thefathomless <dndrew22@gmailc=.com>
2022-07-17 18:27:21 -0500

    Added functionality 	modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/main.rs 	renamed:    src/rustMap.rs -> src/rust_map.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-mod rustMap;
+mod rust_map;
 
 
 fn main() {
-    let ipaddress = std::env::args().nth(1).expect("no pattern given");
-    let port = std::env::args().nth(2).expect("no path given");
-    //Convert ipaddress to str from Stringl
-    rustMap::rust_scan(&ipaddress, &port);
+    let operation = std::env::args().nth(1).expect("No operation choosen");
+    if operation == "scan" {
+        call_rust_scan()
+}
+}
+
+fn call_rust_scan () {
+    let ipaddress = std::env::args().nth(2).expect("No ip given");
+    let port = std::env::args().nth(3).expect("No port given");
+    rust_map::rust_scan(&ipaddress, &port);
 }
--- src/rust_map.rs
+++ src/rust_map.rs
@@ -1,14 +1,16 @@
 use std::net::TcpStream;
-use std::prelude::*;
 
 pub fn rust_scan(ip: &str, port: &str) {
     println!("Connecting to {}:{}", ip, port);
     let scan_target = format!("{}:{}", ip, port);
 
-    if let Ok(Stream) = TcpStream::connect(scan_target) {
+    
+
+    //this works if the port is closed or open but not filtered
+    if let Ok(_stream) = TcpStream::connect(scan_target) {
         println!("{}:{} is open", ip, port);
     } else {
-        println!("{}:{} is either closed or filtered", ip, port);
+        println!("{}:{} is closed", ip, port);
     }
     return();
 }
