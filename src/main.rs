412fa51a8ff61aa576ec864d7e6196acdfa7a314
thefathomless <dndrew22@gmailc=.com>
2022-07-24 10:56:03 -0500

    Added some libraries 	modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 }
 }
 
-fn call_rust_scan () {
+fn call_rust_scan() {
     let ipaddress = std::env::args().nth(2).expect("No ip given");
     let port = std::env::args().nth(3).expect("No port given");
     rust_map::rust_scan(&ipaddress, &port);
