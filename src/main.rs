66a67b29bd822915d95fa63afdae4862c952e06c
thefathomless <dndrew22@gmailc=.com>
2022-07-24 16:26:08 -0500

    Still not working 	modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/main.rs 	modified:   src/rust_map.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 mod rust_map;
 
+// could have the app tui run on a separate thread
 
 fn main() {
     let operation = std::env::args().nth(1).expect("No operation choosen");
     if operation == "scan" {
-        call_rust_scan()
+        call_rust_scan();
 }
 }
 
 fn call_rust_scan() {
     let ipaddress = std::env::args().nth(2).expect("No ip given");
     let port = std::env::args().nth(3).expect("No port given");
-    rust_map::rust_scan(&ipaddress, &port);
+    rust_map::rust_scan(&ipaddress);
 }
--- src/rust_map.rs
+++ src/rust_map.rs
@@ -1,16 +1,25 @@
 use std::net::TcpStream;
+use std::io::prelude::*;
 
-pub fn rust_scan(ip: &str, port: &str) {
+
+pub fn rust_scan(ip: &str) {
+
+    for number in 0..65535 {
+        let port = number.to_string();
+        rust_scan_tcp(ip, port);
+    }
+
+}
+
+
+fn rust_scan_tcp(ip: &str, port: String){
     println!("Connecting to {}:{}", ip, port);
     let scan_target = format!("{}:{}", ip, port);
-
-    
 
     //this works if the port is closed or open but not filtered
     if let Ok(_stream) = TcpStream::connect(scan_target) {
         println!("{}:{} is open", ip, port);
     } else {
-        println!("{}:{} is closed", ip, port);
+        println!("Unable to connect");
     }
-    return();
 }
